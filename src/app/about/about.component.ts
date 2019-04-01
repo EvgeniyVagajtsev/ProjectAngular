9a4324dd89f109c3b425ba76720e778ac9d1dae3
Evgeniy <vagajtsev2011@gmail.com>
2019-04-01 22:14:23 +0400

    console

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,56 +3,44 @@ import * as user from '../user.json';
 
 @Component({
   selector: 'app-about',
-  templateUrl: './about.component.html',
+  template: `<p>Тест</p>
+              <ul>
+                <li *ngFor="let key of user" >
+                  <p>{{key.question}}</p>
+                  <input type="checkbox">{{key.answers}}
+                </li>
+              </ul>
+              <input type="button" value="Показать результат"/> 
+              <input type="reset" value="Сбросить ответы"/>`,
   styleUrls: ['./about.component.css']
 })
+
 export class AboutComponent implements OnInit {
+
+  user = [{
+    "question": "Какая самая большая по населению страна Европы?",
+    "answers": ["Франция", "Великобритания", "Бельгия", "Германия"],
+    "rightAnswer": "Великобритания"
+  }, {
+    "question": "Какая страна находится в центре Европы?",
+    "answers": ["Испания", "Франция", "Беларусь", "Польша"],
+    "rightAnswer": "Беларусь"
+  }, {
+    "question": "Сколько жителей живёт в Беларуси",
+    "answers": ["2млн людей", "1.3млн людей", "1млн людей", "800тыс людей"],
+    "rightAnswer": "1.3млн человек"
+  }, {
+    "question": "Какую страну называют 'Страна зеркальных зданий' ?",
+    "answers": ["Дубаи", "США", "Москва", "Чехия"],
+    "rightAnswer": "CША"
+  }];
 
 
   constructor() {
 
-  }
-
-  check_me(event) {
-    let chbox = document.getElementById("check") as HTMLInputElement;
-    if (chbox.checked) {
-      console.log();
-    } else {
-      console.log("Не выбран");
     }
-  }
-
 
   ngOnInit() {
-    for (let i = 0; i < 4; i++) {
-      let answers1 = document.getElementById("input_" + i);
-      answers1.innerHTML = user.test[0].answers[i];
 
-      let answers2 = document.getElementById("inputs_" + i);
-      answers2.innerHTML = user.test[1].answers[i];
-
-      let answers3 = document.getElementById("inputx_" + i);
-      answers3.innerHTML = user.test[2].answers[i];
-
-      let answers4 = document.getElementById("inputa_" + i);
-      answers4.innerHTML = user.test[3].answers[i];
-
-      for (let k = 0; k < 1; k++) {
-        let inputquestion = document.getElementById("question_" + k);
-        inputquestion.innerHTML = user.test[0].question;
-      for ( k = 1; k < 2; k++) {
-        let inputquestion = document.getElementById("question_" + k);
-         inputquestion.innerHTML = user.test[1].question;
-      for ( k = 2; k < 3; k++) {
-        let inputquestion = document.getElementById("question_" + k);
-          inputquestion.innerHTML = user.test[2].question;
-      for ( k = 3; k < 4; k++) {
-        let inputquestion = document.getElementById("question_" + k);
-          inputquestion.innerHTML = user.test[3].question;
-         }
-        }
-       }
-      }
-    }
   }
 }
